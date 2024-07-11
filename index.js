f0427da12176438fc102ced9e26c070bd58a7f96
jonasKr <yakerdev@gmail.com>
2024-07-11 18:55:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const generate_button = document.getElementById("generate-button"),
+download_bar = document.getElementById('download-bar');
+
+generate_button.addEventListener('click',()=>{
+download_bar.style.display = 'flex';
+})
+
